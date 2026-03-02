670a5811fbb91b1c6b28a26c87168f5f1d767544
Sam Gaus <sam@gaus.co.uk>
2026-03-02 14:25:44 +0000

    Upgrade eslint

---
+++ eslint.config.ts
@@ -0,0 +1,23 @@
+import eslint from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import { defineConfig } from 'eslint/config';
+
+export default defineConfig(
+  { ignores: ['dist'] },
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  reactHooks.configs.flat.recommended,
+  {
+    languageOptions: {
+      globals: globals.browser,
+    },
+    plugins: { 'react-refresh': reactRefresh },
+    rules: {
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'react-hooks/incompatible-library': 'off',
+    },
+  },
+);
